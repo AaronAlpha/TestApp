37c28b93ffb40aa8ac71f73c299785ee6bee8287
Aaron Mac 2026 <reachaaronbernard@gmail.com>
2026-07-04 04:30:50 -0600

    Initial Commit - iOS/MacOS Dev!

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Aaron Bernard on 2026-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}// struct
+
+#Preview {
+    ContentView()
+}// Preview
---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Aaron Bernard on 2026-07-04.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
